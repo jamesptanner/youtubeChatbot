5e5e5ea82c99631a98d7e240f5f064a089efef03
James Tanner <jamesptanner@outlook.com>
2018-01-02 00:24:44 +0000

    think that the gitsecret file needed to update for some reason.

--- src/main/java/uk/co/codingentity/youtubechatbot/Auth.java
+++ src/main/java/uk/co/codingentity/youtubechatbot/Auth.java
@@ -6,14 +6,16 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 public class Auth {
 final static NetHttpTransport NET_TRANSPORT = new NetHttpTransport();
 final static JsonFactory JSON_FACTORY = new JacksonFactory();
-final static Iterable<String> SCOPE = Arrays.asList(";".split(";"));
+    final static Collection<String> SCOPE = Arrays.asList(";".split(";"));
 
     final GoogleAuthorizationCodeFlow flow;
     public Auth(){
-        flow = new GoogleAuthorizationCodeFlow(NET_TRANSPORT, JSON_FACTORY, Secrets.ClientID, Secrets.ClientSecret,SCOPE);
+        flow = new GoogleAuthorizationCodeFlow.Builder(NET_TRANSPORT, JSON_FACTORY, Secrets.ClientID, Secrets.ClientSecret, SCOPE).build();
+
     }
 }
